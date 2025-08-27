474af5aed2df653b20ede0a01568af1d6a0da55b
Mohammad Ali Miyan <143022643+MohammadAliMiyan@users.noreply.github.com>
2025-08-27 13:00:22 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loading-screen");
-  setTimeout(() => {
-    loader.classList.add("hidden");
-  }, 2500); // matches slide-in duration
-});
